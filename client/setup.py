2280ac4a16b73d9c184662033fc9aac621d3e4e0
Lance Hannestad <lhannest@sfu.ca>
2018-09-05 12:58:57 -0700

    Deleted everything in develop

--- client/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = "controller_impl",
-    version = "1.0",
-    author = "lance@starinformatics.com",
-    description = "Implementation of the controller classes of the Rhea beacon",
-    packages = ['controller_impl']
-)
