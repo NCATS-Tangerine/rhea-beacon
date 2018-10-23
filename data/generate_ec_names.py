3086122a35559493b9bba3724d9915cbdd5f945b
Lance Hannestad <lhannest@sfu.ca>
2018-10-23 09:28:53 -0700

    Added data subpackage

---
+++ data/generate_ec_names.py
@@ -0,0 +1,82 @@
+"""
+Download page: ftp://ftp.expasy.org/databases/enzyme
+Explanation of data file and line types: ftp://ftp.expasy.org/databases/enzyme/enzuser.txt
+"""
+
+import requests, os
+
+path = 'ftp://ftp.expasy.org/databases/enzyme/enzyme.dat'
+
+print('Downloading ' + path)
+
+if not os.path.exists('enzyme.dat'):
+    try:
+        import urllib.request
+        urllib.request.urlretrieve(path, 'enzyme.dat')
+    except:
+        import urllib
+        urllib.urlretrieve(path, 'enzyme.dat')
+    finally:
+        if not os.path.exists('enzyme.dat'):
+            quit(f'Could not download {path}')
+
+q="""
+PREFIX rh:<http://rdf.rhea-db.org/>
+PREFIX ch:<http://purl.obolibrary.org/obo/>
+SELECT distinct ?ec WHERE {
+  ?reaction rdfs:subClassOf rh:Reaction .
+  ?reaction rh:ec ?ec .
+}
+order by ?ec
+"""
+
+print('Getting enzymes')
+
+response = requests.get(
+    url="https://sparql.rhea-db.org/sparql",
+    params={
+        'query': q,
+        'format' : 'application/sparql-results+json'
+    }
+)
+
+enzymes = []
+for binding in response.json().get('results').get('bindings'):
+    enzymes.append(binding.get('ec').get('value').replace('http://purl.uniprot.org/enzyme/', ''))
+
+print('Building CSV')
+
+IDENTIFICATION = 'ID'
+TERMINATION = '//'
+ALTERNATE_NAMES = 'AN'
+DESCRIPTION = 'DE'
+
+
+written = 0
+with open('enzyme.dat', 'r') as f:
+    open("ecc_names.csv", "w+").close()
+    with open("ecc_names.csv", "a+") as output_file:
+        output_file.write('ID\tName\tSynonyms\n')
+        d = None
+        for line in f:
+            line_type, line_content = line.split(maxsplit=1)
+
+            if line_type == IDENTIFICATION:
+                d = {'id' : line_content}
+            elif line_type == DESCRIPTION and d is not None:
+                if line_content == 'Deleted entry.':
+                    d = None
+                else:
+                    d['name'] = line_content
+            elif line_type == ALTERNATE_NAMES and d is not None:
+                if 'synonyms' not in d:
+                    d['synonyms'] = list()
+                d['synonyms'].append(line_content)
+            elif line_type == TERMINATION and d is not None:
+                i, n, s = d.get('id', ''), d.get('name', ''), d.get('synonyms', [])
+                s = ';'.join(s)
+                output_file.write(f'{i}\t{n}\t{s}\n')
+
+                written += 1
+
+print('Written:', written, ', Total enzymes in Rhea:', len(enzymes))
