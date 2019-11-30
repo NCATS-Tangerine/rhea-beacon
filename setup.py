95cb6359e22ba8fd2d8dd6093fac7f115a590f4c
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-11-29 17:59:48 -0800

    Update the  BioLink Model to latest

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'BiolinkMG',
+        'biolink-model',
+        'biolinkml',
         'tornado',
         'requests',
         'flask',
